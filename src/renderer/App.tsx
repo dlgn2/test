5bea98933412754f06d8996c53134095005a11a6
Enes Dolgun <58950702+dlgn2@users.noreply.github.com>
2022-07-11 03:10:33 +0300

    Revert "message"
    This reverts commit b58998de203fa9b4e0dd6ab4e1b26c9cb340511c.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,29 +1,50 @@
-import { useMemo, useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { io } from 'socket.io-client';
+import icon from '../../assets/icon.svg';
 import './App.css';
-import Home from './pages/Home';
-import { UserContext } from './Context/UserContext';
-import { SocketContext } from './Context/SocketContext';
-import { GameClientInterface } from './@types/arena';
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  const [socket, setSocket] = useState();
-  const [snackBarError, setSnackBarError] = useState();
-  const [currentSession,setCurrentSession] = useState<GameClientInterface>(null);
-
-  const userValue = useMemo(() => ({ user, setUser,snackBarError,setSnackBarError,currentSession,setCurrentSession}));
-  const socketValue = useMemo(() => ({ socket, setSocket}));
   return (
-    <SocketContext.Provider value={socketValue}>
-      <Router>
-      <UserContext.Provider value={userValue}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        </UserContext.Provider>
-      </Router>
-    </SocketContext.Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
   );
 }
--- src/renderer/components/Topbar/AdSwitch/AdSwitch.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AdSwitch() {
-  return (
-    <div>AdSwitch</div>
-  )
-}
